bd3974f4911e039553657640fb213b8577a63987
kewiany <kewin.czuprynski@gmail.com>
2020-08-20 23:32:30 +0200

    Use TestFactory to create reusable tests, explore kotest
    https://dev.to/kotest/writing-reusable-tests-with-kotest-s-test-factory-5gi

--- logic/src/test/java/xyz/kewiany/contactus/MainLogicTest.kt
+++ logic/src/test/java/xyz/kewiany/contactus/MainLogicTest.kt
@@ -1,18 +1,29 @@
 package xyz.kewiany.contactus
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.freeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 internal class MainLogicTest : FreeSpec({
+    val reusableTest = freeSpec {
+        "test2" - {
+            "assertion2" {
+                MainLogic() shouldNotBe ""
+            }
+        }
+    }
     beforeEach {
         println("before")
     }
     afterEach {
         println("after")
     }
+    include(reusableTest)
     "test" - {
         "assertion" {
             MainLogic() shouldBe true
         }
     }
 })
+
