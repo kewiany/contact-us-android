63ab4f2848ea0adf0e895fda0f3444f446278421
kewiany <kewin.czuprynski@gmail.com>
2020-08-22 21:50:56 +0200

    Simplify code, do not duplicate code

--- logic/src/test/java/xyz/kewiany/contactus/FAQLogicTest.kt
+++ logic/src/test/java/xyz/kewiany/contactus/FAQLogicTest.kt
@@ -5,6 +5,7 @@ import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import xyz.kewiany.contactus.logic.FAQAction
+import xyz.kewiany.contactus.logic.FAQAction.SelectBack
 import xyz.kewiany.contactus.logic.FAQLogic
 import xyz.kewiany.contactus.logic.FAQViewState
 
@@ -14,6 +15,8 @@ internal class FAQLogicTest : CustomFunSpec({
     val state = FAQViewState()
     val stateT = FAQViewStateT(state)
     var logic: Job? = null
+
+    fun getItems() = stateT.items.value
 
     fun run() {
         logic = testScope.launch {
@@ -26,13 +29,13 @@ internal class FAQLogicTest : CustomFunSpec({
 
     test("set two items on init") {
         run()
-        stateT.items.value shouldBe listOf("one", "two")
-        stateT.items.value.count() shouldBe 2
+        getItems() shouldBe listOf("one", "two")
+        getItems().count() shouldBe 2
     }
 
     test("finish logic on select back") {
         run()
-        actions.accept(FAQAction.SelectBack)
+        actions.accept(SelectBack)
         logic?.isCompleted shouldBe true
     }
 })
